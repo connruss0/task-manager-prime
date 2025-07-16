ec48d74cc3f68e049fe956387fa7480df52e2bd7
Connor Russell <connruss0@gmail.com>
2025-07-16 10:56:49 -0600

    Updated directories and seperated out components

--- task-manager-ui/src/api/api.ts
+++ task-manager-ui/src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type {Task} from "./types";
+import type {Task} from "../types/types.ts";
 
 const API_URL = "http://localhost:8080/tasks";
 
---
+++ task-manager-ui/src/components/TaskManagerPage.tsx
@@ -0,0 +1,10 @@
+import {TaskManager} from "./TaskManager.tsx";
+
+export const TaskManagerPage = () => {
+    return (
+        <div className="task-manager">
+        <h1>Task Manager</h1>
+        <TaskManager />
+        </div>
+    );
+}
